5527aa7ea35758dfc00013655a546094f3d82342
dominicxcliu <dominicxcliu@gmail.com>
2016-02-27 01:48:28 -0500

    first commit prototype

---
+++ platformerprototype/Assets/Scripts/DogControls.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DogControls : MonoBehaviour {
+
+
+	public float velocity;
+	public float jumpForce;
+	private Rigidbody myRb;
+	private bool onSomething = false;
+	private bool underSomething = false;
+	private bool movingLeft;
+	private bool movingRight;
+	private bool jump = false;
+	private bool crouching = false;
+	private bool nearEnemy = false;
+	private EnemyBehavior nearestEnemy;
+
+	// Use this for initialization
+	void Start () {
+		velocity = 10f;
+		jumpForce = 1000f;
+		myRb = GetComponent<Rigidbody> ();
+		myRb.freezeRotation = true;
+		Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Dog"), LayerMask.NameToLayer("Interactable"));
+	}
+
+	void Update() {
+		Vector3 right = transform.position + Vector3.right * transform.lossyScale.x * 0.5f;
+		Vector3 left = transform.position - Vector3.right * transform.lossyScale.x * 0.5f;
+
+		Debug.DrawLine (right, right + (Vector3.down * transform.lossyScale.y * 0.5f));
+		Debug.DrawLine (left, left + (Vector3.down * transform.lossyScale.y * 0.5f));
+		Debug.DrawLine (right, right + (Vector3.up * 0.76f));
+		Debug.DrawLine (left, left + (Vector3.up * 0.76f));
+
+		onSomething = Physics.Linecast (right, right + (Vector3.down * transform.lossyScale.y * 0.5f), 1 << LayerMask.NameToLayer ("Obstacle")) 
+			|| Physics.Linecast (left, left + (Vector3.down * transform.lossyScale.y * 0.5f), 1 << LayerMask.NameToLayer ("Obstacle"));
+		
+		underSomething = Physics.Linecast (right, right + (Vector3.up * 0.76f), 1 << LayerMask.NameToLayer ("Obstacle")) 
+			|| Physics.Linecast (left, left + (Vector3.up * 0.76f), 1 << LayerMask.NameToLayer ("Obstacle"));
+
+		movingLeft = false;
+		movingRight = false;
+
+		if (Input.GetKey (KeyCode.A)) {
+			movingLeft = true;
+			Vector3 s = transform.localScale;
+			s.x = -2;
+			transform.localScale = s;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			movingRight = true;
+			Vector3 s = transform.localScale;
+			s.x = 2;
+			transform.localScale = s;
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space) && onSomething && !crouching)
+		{
+			jump = true;
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			crouching = true;
+		}
+		if (!Input.GetKey (KeyCode.S) && !underSomething) {
+			crouching = false;
+		}
+
+		if (Input.GetMouseButtonDown (0)) {
+			if (nearEnemy) {
+				nearestEnemy.isDead = true;
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (movingLeft) {
+			//restrict movement to one plane
+			transform.position = new Vector3 (transform.position.x, transform.position.y, 0f);
+			myRb.velocity = new Vector3 (-1 * velocity, myRb.velocity.y, myRb.velocity.z);
+		}
+		if (movingRight) {
+			transform.position = new Vector3 (transform.position.x, transform.position.y, 0f);
+			myRb.velocity = new Vector3 (velocity, myRb.velocity.y, myRb.velocity.z);
+		}
+
+		if (crouching) {
+			transform.localScale = new Vector3 (transform.localScale.x, 0.5f, 1f);
+			velocity = 5f;
+		}
+		else {
+			transform.localScale = new Vector3 (transform.localScale.x, 1f, 1f);
+			velocity = 10f;
+		}
+
+		if (jump) {
+			myRb.AddForce (Vector3.up * jumpForce);
+//			Vector3 jumpDir = Vector3.up + (myRb.velocity.x / velocity * Vector3.right);
+//			myRb.AddForce (jumpDir * jumpForce);
+			jump = false;
+		}
+//		if (Input.GetKeyUp (KeyCode.D) || Input.GetKeyUp (KeyCode.A)) {
+		if (!movingRight && !movingLeft) {
+			myRb.velocity = new Vector3(0f, myRb.velocity.y, myRb.velocity.z);
+		}
+		transform.rotation = Quaternion.Euler (Vector3.zero);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Enemy") {
+			nearestEnemy = other.gameObject.GetComponent<EnemyBehavior> ();
+			if (!nearestEnemy.isDead) {
+				nearEnemy = true;
+			}
+
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.gameObject.tag == "Enemy") {
+			nearEnemy = false;
+			nearestEnemy = null;
+		}
+	}
+}
---
+++ platformerprototype/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehavior : MonoBehaviour {
+
+	public bool isDead;
+	private int direction;
+	private Rigidbody myRb;
+	public GameObject dog;
+	private bool seesDog = false;
+	private Renderer myRend;
+	private float speed;
+	public float initialSpeed;
+	private float dogAngle;
+	public float initialDogAngle;
+	private float dogDistance;
+	public float initialDogDistance;
+	public float attentionSpan;
+	private float attentionCountdown;
+
+
+	// Use this for initialization
+	void Start () {
+		direction = 1;
+		isDead = false;
+		initialSpeed = 6f;
+		initialDogAngle = 45f;
+		initialDogDistance = 5f;
+		attentionSpan = 2f;
+		attentionCountdown = -1f;
+		speed = initialSpeed;
+		myRb = GetComponent<Rigidbody> ();
+		myRb.freezeRotation = true;
+		myRend = GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isDead) {
+			RaycastHit rh;
+			Vector3 eyePosition = transform.position + Vector3.up * 0.666f;
+			Vector3 dogDirection = dog.transform.position - eyePosition;
+
+			if (seesDog) {
+				dogAngle = 181f;
+				speed = 9f;
+				if (Vector3.Distance (transform.position, dog.transform.position) < 2f) {
+					speed = 0f;
+				}
+				dogDistance = 10f;
+				if (dogDirection.x < 0) {
+					direction = -1;
+				}
+				else {
+					direction = 1;
+				}
+				attentionCountdown = 2f;
+			}
+			else {
+				if (attentionCountdown < 0f) {
+					dogAngle = initialDogAngle;
+					speed = initialSpeed;
+					dogDistance = initialDogDistance;
+				}
+				else {
+					speed = 0f;
+					attentionCountdown -= Time.deltaTime;
+				}
+
+			}
+			seesDog = false;
+			if (Vector3.Angle (dogDirection, Vector3.right * direction + Vector3.down) < dogAngle) {
+				if (Physics.Raycast (eyePosition, dogDirection.normalized * dogDistance, out rh, dogDistance, ~(1 << LayerMask.NameToLayer ("Interactable")))) {
+					if (rh.collider.tag == "Dog") {
+						Debug.Log ("I SAW THE FUCKING DOG");
+						seesDog = true;
+					}
+				}
+			}
+				
+
+			Debug.DrawRay (eyePosition, dogDirection.normalized * dogDistance, Color.green);
+			Debug.DrawRay (eyePosition, Vector3.right * direction * dogDistance, Color.red);
+			Debug.DrawRay (eyePosition, Vector3.down * dogDistance, Color.red);
+			transform.position = new Vector3 (transform.position.x, transform.position.y, 0f);
+			myRb.velocity = new Vector3 (direction * speed, myRb.velocity.y, myRb.velocity.z);
+		}
+		else {
+			transform.rotation = Quaternion.Euler (0f, 0f, 90f);
+		}
+		Vector3 s = transform.localScale;
+		s.x = direction;
+		transform.localScale = s;
+
+
+	}
+
+	void OnCollisionEnter (Collision collision) {
+		if (collision.collider.tag == "Thingy" && !isDead) {
+			direction = direction * -1;
+		}
+	}
+}
