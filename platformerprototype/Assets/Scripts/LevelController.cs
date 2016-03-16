efc178bb8e57c6df6eff8694205e69c3cca3279a
dominicxcliu <dominicxcliu@gmail.com>
2016-03-16 13:14:45 -0400

    added lots of untracked files

---
+++ platformerprototype/Assets/Scripts/LevelController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelController : MonoBehaviour {
+
+	public GameObject dog;
+	public DogControls dogC;
+
+	// Use this for initialization
+	void Start () {
+		dogC = dog.GetComponent<DogControls> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (dogC.isDead) {
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+			}
+		}
+	}
+}
